d0e07c5a2c4fbf5a147e5af40a5089fc85a981b4
Udeme01 <emmanueluddy2@gmail.com>
2025-02-13 16:07:55 +0100

    wrap up on PUG as a template engine

--- app.js
+++ app.js
@@ -5,18 +5,25 @@ const path = require("path");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+app.set("view engine", "pug"); // view engine allows us to tell express that for every dynamic template we're trying to render, please use this engine registered here - in this case 'pug'.
+
+app.set("views", "templates"); // views allows us to tell express where to find this dynamic views - views acts more like a directory.
+
+// NB: now we're telling express that we wanna compile dynamic templates with the 'pug' engine and where to find these templates.
+
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  //   res.status(404).sendFile(path.join(__dirname, "templates", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,15 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
-const rootDir = require("../util/path"); 
+const rootDir = require("../util/path");
+
+const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
-  //   console.log("In another middleware!");
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  //   console.log('shop js', adminData.products);
+  //   res.sendFile(path.join(rootDir, "templates", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { prods: products, pageTitle: "My Shop", path: "/" });
 });
 
 module.exports = router;
- 
